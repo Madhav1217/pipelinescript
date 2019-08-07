0795b980e7f44f85def5a7d444a9d4a48b9679a9
Madhav1217 <madhava1219@gmail.com>
2019-08-07 16:57:40 +0530

    added to access

--- test.sh
+++ test.sh
@@ -3,6 +3,7 @@ aws codepipeline start-pipeline-execution --name fristpipeline >> fristpipeline.
 data=$(grep "pipelineExecutionId" fristpipeline.log)
 pipelineid=$(echo "$data" | sed 's/.*| \(.*\)|/\1/')
 echo $pipelineid
+chmod a+x status.sh
 ./status.sh
 rm -rf fristpipeline.log
 

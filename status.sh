bde860cef5e3d05cabb27506a3d30d96fd879e30
Madhav1217 <madhava1219@gmail.com>
2019-08-07 16:23:39 +0530

    file updated

--- status.sh
+++ status.sh
@@ -11,13 +11,13 @@ do
    if [ $status_value == InProgress ]; then
    return $pipeline
   else
-    if [ $status_value == Succeeded ]: then
+    if [ $status_value == Succeeded ]; then
   echo "Succeeded ->pipelineid: $pipelineid"
         echo $(grep "status" status_data.log)
         mv status_data.log bkstatus_data.log
         exit 0
 	else
-      if [ $status_value == Failed ]: then
+      if [ $status_value == Failed ]; then
 	echo "Failed ->pipelineid: $pipelineid"
         echo $(grep "status" status_data.log)
         mv status_data.log bkstatus_data.log

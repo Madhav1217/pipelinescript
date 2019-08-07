8039f4c85248602069c228a78f79d4680f0f3aa7
Madhav1217 <madhava1219@gmail.com>
2019-08-07 22:04:46 +0530

    new file updated

--- test.sh
+++ test.sh
@@ -1,13 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+aws configure set aws_access_key_id AKIAI2L5XM3WEEKX7EZA
+aws configure set aws_secret_access_key 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
+aws configure set default.region us-east-2
+aws configure set output_format table
 aws codepipeline start-pipeline-execution --name fristpipeline >> fristpipeline.log 2>&1
 data=$(grep "pipelineExecutionId" fristpipeline.log)
 pipelineid=$(echo "$data" | sed 's/.*| \(.*\)|/\1/')
-echo $pipelineid
-pipelineid=$1
+rm -rf fristpipeline.log
 echo $pipelineid
 while true
 do
-   aws codepipeline get-pipeline-state --name fristPipeline >> status_data.log 2>&1
+   aws codepipeline get-pipeline-execution --pipeline-name fristpipeline --pipeline-execution-id $pipelineid >> status_data.log 2>&1
    cat status_data.log
    status_value=$(grep "InProgress" status_data.log)
    echo $status_value
@@ -16,10 +19,8 @@ do
         echo $(grep "status" status_data.log)
         mv status_data.log bkstatus_data.log
         break
+
    fi
    sleep 60
    mv status_data.log bkstatus_data.log
 done
-rm -rf fristpipeline.log
-
-

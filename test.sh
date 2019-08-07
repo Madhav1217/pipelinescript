83d3976a251d620823d106156ca0dcc18bebce0c
Madhav1217 <madhava1219@gmail.com>
2019-08-07 15:47:51 +0530

    modified test.sh file

--- test.sh
+++ test.sh
@@ -3,24 +3,7 @@ aws codepipeline start-pipeline-execution --name fristpipeline >> fristpipeline.
 data=$(grep "pipelineExecutionId" fristpipeline.log)
 pipelineid=$(echo "$data" | sed 's/.*| \(.*\)|/\1/')
 echo $pipelineid
-pipelineid=$1
-echo $pipelineid
-while true
-do
-   aws codepipeline get-pipeline-execution --pipeline-name fristpipeline --pipeline-execution-id $pipelineid >> status_data.log 2>&1
-   cat status_data.log
-   status_value=$(grep "InProgress" status_data.log)
-   echo $status_value
-   if [ -z "$status_value" ]; then
-        echo "successfully completed ->pipelineid: $pipelineid"
-        echo $(grep "status" status_data.log)
-        mv status_data.log bkstatus_data.log
-        break
-   fi
-   sleep 60
-   mv status_data.log bkstatus_data.log
-done
+bash status.sh
 rm -rf fristpipeline.log
-
 
 
